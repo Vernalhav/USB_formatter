583ce38efc40184efdfd2d63f8a376c37a2db81c
Victor <40337396+Vernalhav@users.noreply.github.com>
2019-11-09 23:19:08 -0300

    fixed formatting

--- formatter.c
+++ formatter.c
@@ -4,28 +4,28 @@
 
 #define USB_PATH			"/dev/sdb"
 
-#define OS_NAME 			"myformat"		// 8 BYTES
-#define BYTES_PER_SEC 		 512			// 2 BYTES
-#define SECTORS_PER_CLUSTER  16				// 1 BYTE
-#define RESERVED_SECTORS 	 2				// 2 BYTES
-#define FAT_COPIES 			 1				// 1 BYTE
-#define MEDIA_DESCRIPTOR 	 0xf8			// 1 BYTE
-#define SECTORS_PER_TRACK 	 512			// 2 BYTES
-#define DISK_HEADS			 4				// 2 BYTES
-#define FAT_SECTOR_OFFSET	 2??			// 4 BYTES
-#define SECTORS_IN_PARTITION 512??			// 4 BYTES
-#define SECTORS_PER_FAT		 512??			// 4 BYTES
-#define FAT_FLAGS			 ??				// 2 BYTES
-#define DRIVE_VERSION		 ??				// 2 BYTES
-#define ROOT_CLUSTER		 ??				// 4 BYTES
-#define LOGIC_INFO_CSECTOR	 ??				// 2 BYTES
-#define LOGIC_BACKUP_SECTOR	 ??				// 2 BYTES
-#define LOGIC_DRIVE_NUMBER	 0x80			// 1 BYTE
-#define CURRENT_HEAD		 ??				// 1 BYTE
-#define SIGNATURE			 0x28				// 1 BYTE
-#define SERIAL_NUMBER		 ?? 			// 4 BYTES
+#define OS_NAME 		 "myformat"	// 8 BYTES
+#define BYTES_PER_SEC 		 512		// 2 BYTES
+#define SECTORS_PER_CLUSTER   	 16		// 1 BYTE
+#define RESERVED_SECTORS 	 2		// 2 BYTES
+#define FAT_COPIES 		 1		// 1 BYTE
+#define MEDIA_DESCRIPTOR 	 0xf8		// 1 BYTE
+#define SECTORS_PER_TRACK 	 512		// 2 BYTES
+#define DISK_HEADS		 4		// 2 BYTES
+#define FAT_SECTOR_OFFSET	 2??		// 4 BYTES
+#define SECTORS_IN_PARTITION	 512??		// 4 BYTES
+#define SECTORS_PER_FAT		 512??		// 4 BYTES
+#define FAT_FLAGS		 ??		// 2 BYTES
+#define DRIVE_VERSION		 ??		// 2 BYTES
+#define ROOT_CLUSTER		 ??		// 4 BYTES
+#define LOGIC_INFO_CSECTOR	 ??		// 2 BYTES
+#define LOGIC_BACKUP_SECTOR	 ??		// 2 BYTES
+#define LOGIC_DRIVE_NUMBER	 0x80		// 1 BYTE
+#define CURRENT_HEAD		 ??		// 1 BYTE
+#define SIGNATURE		 0x28		// 1 BYTE
+#define SERIAL_NUMBER		 ?? 		// 4 BYTES
 #define VOLUME_LABEL		"USPSTICK   "	// 11 BYTES
-#define SYSTEM_ID			"FAT32   "		// 8 BYTES
+#define SYSTEM_ID		"FAT32   "	// 8 BYTES
 
 /*
 	Specification Links
