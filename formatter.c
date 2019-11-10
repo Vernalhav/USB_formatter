7ceeaaa1fdcf786bc6bd60aa59210b2f5fc75d45
vernalhav <vernalhav@usp.br>
2019-11-10 14:39:43 -0300

    Finished parameters

--- formatter.c
+++ formatter.c
@@ -2,30 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define USB_PATH		"/dev/sdb"
 
-#define OS_NAME 		 "myformat"	// 8 BYTES
-#define BYTES_PER_SEC 		 512		// 2 BYTES
-#define SECTORS_PER_CLUSTER   	 16		// 1 BYTE
-#define RESERVED_SECTORS 	 2		// 2 BYTES
-#define FAT_COPIES 		 1		// 1 BYTE
-#define MEDIA_DESCRIPTOR 	 0xf8		// 1 BYTE
-#define SECTORS_PER_TRACK 	 512		// 2 BYTES
-#define DISK_HEADS		 4		// 2 BYTES
-#define FAT_SECTOR_OFFSET	 2??		// 4 BYTES
-#define SECTORS_IN_PARTITION	 512??		// 4 BYTES
-#define SECTORS_PER_FAT		 512??		// 4 BYTES
-#define FAT_FLAGS		 ??		// 2 BYTES
-#define DRIVE_VERSION		 ??		// 2 BYTES
-#define ROOT_CLUSTER		 ??		// 4 BYTES
-#define LOGIC_INFO_CSECTOR	 ??		// 2 BYTES
-#define LOGIC_BACKUP_SECTOR	 ??		// 2 BYTES
-#define LOGIC_DRIVE_NUMBER	 0x80		// 1 BYTE
-#define CURRENT_HEAD		 ??		// 1 BYTE
-#define SIGNATURE		 0x28		// 1 BYTE
-#define SERIAL_NUMBER		 ?? 		// 4 BYTES
-#define VOLUME_LABEL		"USPSTICK   "	// 11 BYTES
-#define SYSTEM_ID		"FAT32   "	// 8 BYTES
+
+#define USB_PATH			"/dev/sdb"	// MAKE SURE THIS IS THE CORRECT PATH
+#define MAX_STORAGE_GIG		4
+
+#define BYTES_IN_GIG		1073741824	// Should not be changed
+#define MAX_BYTES			MAX_STORAGE_GIG*BYTES_IN_GIG
+
+#define OS_NAME 		 	"myformat"	// 8 BYTES
+#define BYTES_PER_SEC 		512			// 2 BYTES
+#define SECTORS_PER_CLUSTER 16			// 1 BYTE
+#define RESERVED_SECTORS 	2			// 2 BYTES
+#define NUM_FATS 		 	2			// 1 BYTE
+#define ROOT_DIR_ENTRIES	0			// 2 BYTES (must be 0)
+#define TOTAL_SECTORS_16	0			// 2 BYTES (must be 0)
+
+#define MEDIA_DESCRIPTOR 	0xf8		// 1 BYTE
+#define FAT16_SIZE			0			// 2 BYTES (must be 0)
+
+#define SECTORS_PER_TRACK 	512			// 2 BYTES
+#define DISK_HEADS		 	4			// 2 BYTES
+#define HIDDEN_SECTORS	 	0			// 4 BYTES
+#define TOTAL_SECTORS_32 	MAX_BYTES/BYTES_PER_SEC	// 4 BYTES
+
+#define FAT32_SIZE			TOTAL_SECTORS_32/2 - 1	// 4 BYTES
+#define FAT_FLAGS		 	0x80		// 2 BYTES
+#define FAT_VERSION			0			// 2 BYTES
+#define ROOT_CLUSTER		1 + RESERVED_SECTORS// 4 BYTES
+#define LOGIC_INFO_SECTOR	1			// 2 BYTES
+#define LOGIC_BACKUP_SECTOR	0			// 2 BYTES
+#define RESERVED 			(long long)0// 12 BYTES (must be 0)
+
+#define LOGIC_DRIVE_NUMBER	0x80		// 1 BYTE
+#define SIGNATURE		 	0x29		// 1 BYTE
+#define SERIAL_NUMBER		0x1010 		// 4 BYTES
+#define VOLUME_LABEL		"USPSTICK   "// 11 BYTES
+#define SYSTEM_ID			"FAT32   "	// 8 BYTES
 
 /*
 	Specification Links
